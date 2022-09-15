7cf2530835cd8109321816d827f4416536612cfe
IKE37 <92145145+IKE37@users.noreply.github.com>
2022-09-15 14:28:56 +0700

    first commit

---
+++ Assets/New Folder/New Folder/Scripts/bulletHit.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletHit : MonoBehaviour {
+
+	projectileController myPC;
+
+	public GameObject bulletExplosion;
+
+	void Awake(){
+		myPC = GetComponentInParent<projectileController> ();
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.gameObject.tag == "Shootable") {
+			myPC.removeForce ();
+			Instantiate (bulletExplosion, transform.position, transform.rotation);
+			Destroy (gameObject);
+		}
+	}
+
+	void OnTringgerStay2D(Collider2D other){
+		if (other.gameObject.tag == "Shootable") {
+			myPC.removeForce ();
+			Instantiate (bulletExplosion, transform.position, transform.rotation);
+			Destroy (gameObject);
+		}
+	}
+}
